4e1bb62193eeaa89efe312c7c69bbc336e8eaa87
zarpe3@hotmail.com <zarpe3@hotmail.com>
2023-02-23 14:13:58 +0000

    remove comment

--- app/Actions/Asterisk/GetTrunks.php
+++ app/Actions/Asterisk/GetTrunks.php
@@ -19,7 +19,6 @@ class GetTrunks
         $response = Http::post('http://webdec-dev03.webdec.com.br/trunks/list', [
           'accountcode' => $this->actionRecord->accountcode,
         ]);
-        //\Log::info($response->json());
 
         return $response->json();
     }
